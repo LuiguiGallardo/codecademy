184e8e637bac346e101208a41c1ad68cd14d80b2
Luigui Gallardo-Becerra <luigui@Luiguis-MacBook-Air.local>
2021-12-01 12:09:07 -0600

    Recursion lesson completed.

---
+++ cs102/07_recursion/recursion_project_tree.py
@@ -0,0 +1,27 @@
+# Define build_bst() below...
+def build_bst(my_list):
+  if len(my_list) == 0:
+    return "No Child"
+
+  middle_idx = len(my_list)//2
+  middle_value = my_list[middle_idx]
+  print("Middle index: " + str(middle_idx))
+  print("Middle value: " + str(middle_value))
+
+  tree_node = {"data": middle_value}
+  left_half = my_list[: middle_idx]
+  right_half = my_list[middle_idx + 1:]
+  tree_node["left_child"] = build_bst(left_half)
+  tree_node["right_child"] = build_bst(right_half)
+
+  return tree_node
+
+# For testing
+sorted_list = [12, 13, 14, 15, 16]
+binary_search_tree = build_bst(sorted_list)
+print(binary_search_tree)
+
+# fill in the runtime as a string
+# 1, logN, N, N*logN, N^2, 2^N, N!
+runtime = "N*logN"
+
